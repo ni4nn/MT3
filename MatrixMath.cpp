9755bc03509d74b44243bf0f6c98033c93664a08
ni4nn <146914397+ni4nn@users.noreply.github.com>
2024-06-26 19:49:44 +0900

    完成

--- MatrixMath.cpp
+++ MatrixMath.cpp
@@ -2,17 +2,65 @@
 
 Matrix4x4 MatrixMath::Add(const Matrix4x4& m1, const Matrix4x4& m2)
 {
-    return Matrix4x4();
+    Matrix4x4 result{};
+
+    result.m[0][0] = m1.m[0][0] + m2.m[0][0];
+    result.m[0][1] = m1.m[0][1] + m2.m[0][1];
+    result.m[0][2] = m1.m[0][2] + m2.m[0][2];
+    result.m[0][3] = m1.m[0][3] + m2.m[0][3];
+    result.m[1][0] = m1.m[1][0] + m2.m[1][0];
+    result.m[1][1] = m1.m[1][1] + m2.m[1][1];
+    result.m[1][2] = m1.m[1][2] + m2.m[1][2];
+    result.m[1][3] = m1.m[1][3] + m2.m[1][3];
+    result.m[2][0] = m1.m[2][0] + m2.m[2][0];
+    result.m[2][1] = m1.m[2][1] + m2.m[2][1];
+    result.m[2][2] = m1.m[2][2] + m2.m[2][2];
+    result.m[2][3] = m1.m[2][3] + m2.m[2][3];
+    result.m[3][0] = m1.m[3][0] + m2.m[3][0];
+    result.m[3][1] = m1.m[3][1] + m2.m[3][1];
+    result.m[3][2] = m1.m[3][2] + m2.m[3][2];
+    result.m[3][3] = m1.m[3][3] + m2.m[3][3];
+
+
+    return result;
 }
 
 Matrix4x4 MatrixMath::Subtract(const Matrix4x4& m1, const Matrix4x4& m2)
 {
-    return Matrix4x4();
+    Matrix4x4 result{};
+
+    result.m[0][0] = m1.m[0][0] - m2.m[0][0];
+    result.m[0][1] = m1.m[0][1] - m2.m[0][1];
+    result.m[0][2] = m1.m[0][2] - m2.m[0][2];
+    result.m[0][3] = m1.m[0][3] - m2.m[0][3];
+    result.m[1][0] = m1.m[1][0] - m2.m[1][0];
+    result.m[1][1] = m1.m[1][1] - m2.m[1][1];
+    result.m[1][2] = m1.m[1][2] - m2.m[1][2];
+    result.m[1][3] = m1.m[1][3] - m2.m[1][3];
+    result.m[2][0] = m1.m[2][0] - m2.m[2][0];
+    result.m[2][1] = m1.m[2][1] - m2.m[2][1];
+    result.m[2][2] = m1.m[2][2] - m2.m[2][2];
+    result.m[2][3] = m1.m[2][3] - m2.m[2][3];
+    result.m[3][0] = m1.m[3][0] - m2.m[3][0];
+    result.m[3][1] = m1.m[3][1] - m2.m[3][1];
+    result.m[3][2] = m1.m[3][2] - m2.m[3][2];
+    result.m[3][3] = m1.m[3][3] - m2.m[3][3];
+
+    return result;
 }
 
 Matrix4x4 MatrixMath::Multiply(const Matrix4x4& m1, const Matrix4x4& m2)
 {
-    return Matrix4x4();
+    Matrix4x4 result = {};
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            for (int k = 0; k < 4; k++) {
+                result.m[i][j] += m1.m[i][k] * m2.m[k][j];
+            }
+        }
+    }
+
+    return result;
 }
 
 Matrix4x4 MatrixMath::Inverse(const Matrix4x4& m)
@@ -110,10 +158,22 @@ Matrix4x4 MatrixMath::Inverse(const Matrix4x4& m)
 
 Matrix4x4 MatrixMath::Transpose(const Matrix4x4 m)
 {
-    return Matrix4x4();
+    Matrix4x4 result{
+    m.m[0][0],m.m[1][0],m.m[2][0],m.m[3][0],
+    m.m[0][1],m.m[1][1],m.m[2][1],m.m[3][1],
+    m.m[0][2],m.m[1][2],m.m[2][2],m.m[3][2],
+    m.m[0][3],m.m[1][3],m.m[2][3],m.m[3][3]
+    };
+    return result;
 }
 
 Matrix4x4 MatrixMath::MakeIdentity4x4()
 {
-    return Matrix4x4();
+    Matrix4x4 result{
+     1,0,0,0,
+     0,1,0,0,
+     0,0,1,0,
+     0,0,0,1
+    };
+    return result;
 }
