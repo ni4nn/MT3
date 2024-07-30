929cca30dd02d2dd4b2b2c5165b93e5c9dc86b63
ni4nn <146914397+ni4nn@users.noreply.github.com>
2024-07-30 10:22:00 +0900

    修正

--- MatrixMath.cpp
+++ MatrixMath.cpp
@@ -426,7 +426,7 @@ Matrix4x4 MatrixMath::MakeViewportMatrix(float left, float top, float width, flo
     result.m[0][2] = 0.0f;;
     result.m[0][3] = 0.0f;;
     result.m[1][0] = 0.0f;;
-    result.m[1][1] = height / 2;
+    result.m[1][1] = -(height / 2);
     result.m[1][2] = 0.0f;
     result.m[1][3] = 0.0f;
     result.m[2][0] = 0.0f;;
